25bae841d84abb46c0e30b0f54ab228f80b48609
Xin Wu <iridescent330@gmail.com>
2017-02-14 19:42:29 -0800

    Basic Interaction
    Add basic interaction

---
+++ Assets/Scripts/Monitor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NewtonVR;
+using UnityEngine;
+
+public class Monitor : MonoBehaviour {
+
+    public NVRButton Button;
+    public GameObject MonitorContent;
+    public GameObject IndicatorLight;
+    public GameObject PowerAdapter;
+
+    private NVRAttachPoint attachPointScript;
+
+    private void Start()
+    {
+        attachPointScript = PowerAdapter.GetComponent<NVRAttachPoint>();
+    }
+
+    private void Update()
+    {
+        if (attachPointScript.IsAttached)
+        {
+            Material greenMaterial = Resources.Load("Materials/greenlight", typeof(Material)) as Material;
+            IndicatorLight.GetComponent<Renderer>().material = greenMaterial;
+        }
+
+        if (Button.ButtonDown && attachPointScript.IsAttached)
+        {
+            Material contentMaterial = Resources.Load("Materials/monitorcontent", typeof(Material)) as Material;
+            MonitorContent.GetComponent<Renderer>().material = contentMaterial;
+        }
+    }
+}

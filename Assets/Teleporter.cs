f3be89c9a3b1394404bf614c589b721754664a06
Kristin Dragos <KristinDragos@gmail.com>
2017-02-16 19:52:55 -0800

    Added basic room movement via portals.
    FEATURE: User enters a portal and is transported to one of the ohter rooms. Past or future.

---
+++ Assets/Teleporter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Teleporter : MonoBehaviour {
+	public GameObject teleportLocation;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		// Move player to the teleportLocation;
+		this.player.transform.position = teleportLocation.transform.position;
+		// Deactivate the current teleporter so only one player can go through.
+		gameObject.SetActive(false);
+
+	}
+
+}

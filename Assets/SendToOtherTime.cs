7e1db721cbe2f3148aeba57070c4e6eba1862bc4
Kristin Dragos <KristinDragos@gmail.com>
2017-02-18 13:50:11 -0800

    Added teleporter table
    FEATURE: Items with the teleportable script attached can now be teleported
    to the other "room" when placed on the center of the table.

---
+++ Assets/SendToOtherTime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendToOtherTime : MonoBehaviour {
+	public GameObject otherTeleporterSpawnPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider coll) {
+		if (!coll.GetComponent<Teleportable> ().teleported) {
+			coll.GetComponent<Teleportable> ().teleported = true;
+			coll.transform.position = otherTeleporterSpawnPoint.transform.position;
+		}
+	}
+}

08f7e8ebe8d999c30f18013596798b20f08d4a67
Xin Wu <iridescent330@gmail.com>
2017-02-21 22:37:45 -0800

    Finished water tank puzzle
    Added electric bar

---
+++ Assets/Scripts/ElectricBar.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ElectricBar : MonoBehaviour {
+
+    public RectTransform powerTransform;
+    public float CurrentHeadPower
+    {
+        get { return currentHeadPower; }
+        set {
+            currentHeadPower = value;
+            HandleHeadth();
+        }
+    }
+    public float CurrentColorPower
+    {
+        get { return currentColorPower; }
+        set
+        {
+            currentColorPower = value;
+            HandleHeadth();
+        }
+    }
+    public float CurrentTailPower
+    {
+        get { return currentTailPower; }
+        set
+        {
+            currentTailPower = value;
+            HandleHeadth();
+        }
+    }
+
+    public Text powerText;
+    public Image visualPower;
+
+    private float cachedY;
+    private float cachedZ;
+    private float minXValue;
+    private float maxXValue;
+    private float currentHeadPower;
+    private float currentColorPower;
+    private float currentTailPower;
+    private int maxPower;
+
+    // Use this for initialization
+    void Start () {
+        cachedY = powerTransform.position.y;
+        cachedZ = powerTransform.position.z;
+        maxXValue = powerTransform.position.x;
+        minXValue = powerTransform.position.x - powerTransform.rect.width;
+        maxPower = 100;
+        currentHeadPower = 0f;
+        currentColorPower = 0f;
+        currentTailPower = 0f;
+        HandleHeadth();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void HandleHeadth() {
+
+        float curPower = currentHeadPower + currentColorPower + currentTailPower;
+
+        powerText.text = "Power: " + (int)curPower;
+
+        float currentXValue = MapValues(curPower, 0, maxPower, minXValue, maxXValue);
+
+        powerTransform.position = new Vector3(currentXValue, cachedY, cachedZ);
+
+        if (curPower > maxPower / 2)
+        {
+            visualPower.color = new Color32((byte)MapValues(curPower, maxPower/2, maxPower, 255, 0), 255, 0, 255);
+        }
+        else {
+            visualPower.color = new Color32(255, (byte)MapValues(curPower, 0, maxPower/2, 0, 255), 0, 255);
+        }
+    }
+
+    private float MapValues(float x, float inMin, float inMax, float outMin, float outMax) {
+        return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+}
--- Assets/Scripts/KnobControl.cs
+++ Assets/Scripts/KnobControl.cs
@@ -18,15 +18,11 @@ public class KnobControl : MonoBehaviour {
     public ParticleSystem tailParticle;
 
     public GameObject eel;
+    public GameObject electricBar;
 
     private int lastHeadAngle = 360;
     private int lastColorAngle = 360;
     private int lastTailAngle = 360;
-
-    private void Awake()
-    {
-        
-    }
 
     private void Update()
     {
@@ -83,7 +79,7 @@ public class KnobControl : MonoBehaviour {
             headParticle.Stop();
             if (headParticle.isStopped)
             {
-                StartCoroutine(updateEelHead(headAngle));
+                updateEelHead(headAngle);
             }
         }
 
@@ -97,7 +93,7 @@ public class KnobControl : MonoBehaviour {
             colorParticle.Stop();
             if (colorParticle.isStopped)
             {
-                StartCoroutine(updateEelColor(colorAngle));
+                updateEelColor(colorAngle);
             }
         }
 
@@ -109,93 +105,240 @@ public class KnobControl : MonoBehaviour {
         else
         {
             tailParticle.Stop();
+            if(tailParticle.isStopped)
+            {
+                updateEelTail(tailAngle);
+            }
         }
     }
 
-    private IEnumerator updateEelHead(float headAngle)
+    private void updateEelHead(float headAngle)
     {
         int head = (int)((360 - headAngle) / 36);
 
-        yield return new WaitForSeconds(1);
+        //yield return new WaitForSeconds(1);
 
         switch (head)
         {
             case 0:
                 eel.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                if (electricBar.GetComponent<ElectricBar>().CurrentHeadPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentHeadPower -= 0.1f;
+                }
                 break;
             case 1:
                 eel.transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
+                if (electricBar.GetComponent<ElectricBar>().CurrentHeadPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentHeadPower -= 0.1f;
+                }
                 break;
             case 2:
                 eel.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+                if (electricBar.GetComponent<ElectricBar>().CurrentHeadPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentHeadPower -= 0.1f;
+                }
                 break;
             case 3:
                 eel.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+                if (electricBar.GetComponent<ElectricBar>().CurrentHeadPower < 33) {
+                    electricBar.GetComponent<ElectricBar>().CurrentHeadPower += 0.1f;
+                }
                 break;
             case 4:
                 eel.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+                if (electricBar.GetComponent<ElectricBar>().CurrentHeadPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentHeadPower -= 0.1f;
+                }
                 break;
             case 5:
                 eel.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
+                if (electricBar.GetComponent<ElectricBar>().CurrentHeadPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentHeadPower -= 0.1f;
+                }
                 break;
             case 6:
                 eel.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+                if (electricBar.GetComponent<ElectricBar>().CurrentHeadPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentHeadPower -= 0.1f;
+                }
                 break;
             case 7:
                 eel.transform.localScale = new Vector3(0.45f, 0.45f, 0.45f);
+                if (electricBar.GetComponent<ElectricBar>().CurrentHeadPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentHeadPower -= 0.1f;
+                }
                 break;
             case 8:
                 eel.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                if (electricBar.GetComponent<ElectricBar>().CurrentHeadPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentHeadPower -= 0.1f;
+                }
                 break;
             case 9:
                 eel.transform.localScale = new Vector3(0.55f, 0.55f, 0.55f);
+                if (electricBar.GetComponent<ElectricBar>().CurrentHeadPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentHeadPower -= 0.1f;
+                }
                 break;
         }
     }
 
-    private IEnumerator updateEelColor(float colorAngle) {
+    private void updateEelColor(float colorAngle) {
         int color = (int)((360 - colorAngle) / 36);
-        yield return new WaitForSeconds(1);
+        //yield return new WaitForSeconds(1);
 
         switch (color)
         {
             case 0:
                 eel.GetComponent<Renderer>().material.color = Color.cyan;
+                if (electricBar.GetComponent<ElectricBar>().CurrentColorPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentColorPower -= 0.1f;
+                }
                 break;
             case 1:
                 eel.GetComponent<Renderer>().material.color = Color.red;
+                if (electricBar.GetComponent<ElectricBar>().CurrentColorPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentColorPower -= 0.1f;
+                }
                 break;
             case 2:
                 eel.GetComponent<Renderer>().material.color = Color.blue;
+                if (electricBar.GetComponent<ElectricBar>().CurrentColorPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentColorPower -= 0.1f;
+                }
                 break;
             case 3:
                 eel.GetComponent<Renderer>().material.color = Color.black;
+                if (electricBar.GetComponent<ElectricBar>().CurrentColorPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentColorPower -= 0.1f;
+                }
                 break;
             case 4:
                 eel.GetComponent<Renderer>().material.color = Color.white;
+                if (electricBar.GetComponent<ElectricBar>().CurrentColorPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentColorPower -= 0.1f;
+                }
                 break;
             case 5:
                 eel.GetComponent<Renderer>().material.color = Color.yellow;
+                if (electricBar.GetComponent<ElectricBar>().CurrentColorPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentColorPower -= 0.1f;
+                }
                 break;
             case 6:
-                eel.GetComponent<Renderer>().material.color = Color.gray;
+                eel.GetComponent<Renderer>().material.color = Color.green;
+                if (electricBar.GetComponent<ElectricBar>().CurrentColorPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentColorPower -= 0.1f;
+                }
                 break;
             case 7:
-                eel.GetComponent<Renderer>().material.color = Color.grey;
+                eel.GetComponent<Renderer>().material.color = Color.gray;
+                if (electricBar.GetComponent<ElectricBar>().CurrentColorPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentColorPower -= 0.1f;
+                }
                 break;
             case 8:
-                eel.GetComponent<Renderer>().material.color = Color.green;
+                eel.GetComponent<Renderer>().material.color = Color.grey;
+                if (electricBar.GetComponent<ElectricBar>().CurrentColorPower < 33)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentColorPower += 0.1f;
+                }
                 break;
             case 9:
                 eel.GetComponent<Renderer>().material.color = Color.magenta;
+                if (electricBar.GetComponent<ElectricBar>().CurrentColorPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentColorPower -= 0.1f;
+                }
                 break;
         }
     }
 
-    private IEnumerator updateEelTail(float tailAngle)
+    private void updateEelTail(float tailAngle)
     {
         int tail = (int)((360 - tailAngle) / 36);
 
-        yield return new WaitForSeconds(1);
+        //yield return new WaitForSeconds(1);
+
+        switch (tail)
+        {
+            case 0:
+                if (electricBar.GetComponent<ElectricBar>().CurrentTailPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentTailPower -= 0.1f;
+                }
+                break;
+            case 1:
+                if (electricBar.GetComponent<ElectricBar>().CurrentTailPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentTailPower -= 0.1f;
+                }
+                break;
+            case 2:
+                if (electricBar.GetComponent<ElectricBar>().CurrentTailPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentTailPower -= 0.1f;
+                }
+                break;
+            case 3:
+                if (electricBar.GetComponent<ElectricBar>().CurrentTailPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentTailPower -= 0.1f;
+                }
+                break;
+            case 4:
+                if (electricBar.GetComponent<ElectricBar>().CurrentTailPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentTailPower -= 0.1f;
+                }
+                break;
+            case 5:
+                if (electricBar.GetComponent<ElectricBar>().CurrentTailPower < 34)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentTailPower += 0.1f;
+                }
+                break;
+            case 6:
+                if (electricBar.GetComponent<ElectricBar>().CurrentTailPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentTailPower -= 0.1f;
+                }
+                break;
+            case 7:
+                if (electricBar.GetComponent<ElectricBar>().CurrentTailPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentTailPower -= 0.1f;
+                }
+                break;
+            case 8:
+                if (electricBar.GetComponent<ElectricBar>().CurrentTailPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentTailPower -= 0.1f;
+                }
+                break;
+            case 9:
+                if (electricBar.GetComponent<ElectricBar>().CurrentTailPower > 0)
+                {
+                    electricBar.GetComponent<ElectricBar>().CurrentTailPower -= 0.1f;
+                }
+                break;
+        }
     }
 }
